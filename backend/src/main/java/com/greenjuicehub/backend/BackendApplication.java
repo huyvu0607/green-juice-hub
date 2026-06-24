74534820323fd980bc5490423b1fa17a1f33285d
huyvu0607 <vudh1092@ut.edu.vn>
2026-06-24 16:28:44 +0700

    Add @EnableSpringDataWebSupport

--- backend/src/main/java/com/greenjuicehub/backend/BackendApplication.java
+++ backend/src/main/java/com/greenjuicehub/backend/BackendApplication.java
@@ -2,12 +2,14 @@ package com.greenjuicehub.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class BackendApplication {
 
 	public static void main(String[] args) {
